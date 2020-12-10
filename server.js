e66a4fdb220fcd09c1df3d91427f058ef8b420e2
Your Name <you@example.com>
2020-12-10 09:01:21 +0700

    tambah function

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use('/', express.static('./public'));
 
 const DB_CONFIG = {
     user: "bootcamp",
-    password: "bootcamp*123",
+    password: "Bootcamp*123",
     server: "206.189.80.195",
     database:"populasi"
 }
@@ -34,3 +34,44 @@ var WithDB = function WithDB(req,res,next){
 }
 
 app.get('/db',WithDB)
+
+app.post('/simpan-data', function(req, res){
+
+    const { namaMahasiswa, mataKuliah, nilai } = req.body;
+
+    new sql.ConnectionPool(DB_CONFIG).connect().then(pool => {
+        const sql = `
+            INSERT INTO populasi.dbo.ari_panji 
+            (NamaMahasiswa, MataKuliah, Nilai, Idx, Status)
+            VALUES ('${namaMahasiswa}', '${mataKuliah}', '${nilai}', 
+            CASE WHEN Nilai < '20' THEN 'E'
+            WHEN Nilai < '40' THEN 'D'
+            WHEN Nilai < '60' THEN 'C'
+            WHEN Nilai < '80' THEN 'B'
+            WHEN Nilai < '100' THEN 'A'
+            END,
+            CASE WHEN Idx < '60' THEN 'Tidak Lulus'
+           	ELSE 'Lulus' END);`
+
+        return pool.query(sql)
+    }).then(result => {
+        return res.send(result)
+    }).catch(err => {
+        return res.send(err)
+    })
+
+})
+
+app.delete('/hapus-data/:id',  (req, res) => {
+    const { id } = req.params
+    new sql.ConnectionPool(DB_CONFIG).connect().then(pool => {
+        const sql = `
+            Delete from populasi.dbo.ari_panji where id = '${id}'`
+
+        return pool.query(sql)
+    }).then(result => {
+        return res.send(result)
+    }).catch(err => {
+        return res.send(err)
+    })
+});
